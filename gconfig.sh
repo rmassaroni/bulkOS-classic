49df54adf61918a8a7855640e4f865e9170febe3
rjm <robjmass@gmail.com>
2024-02-24 21:08:33 -0500

    gconfig

---
+++ gconfig.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+auto_glink=true
+require_gcopies=false #require directory 'gcopies' to put copes in. if gcopies exists, copies will go there even if this is turned off
+unhide=true #removes "." in copied files
+
+gprint-settings() {
+    echo "auto_glink: $auto_glink"
+}
